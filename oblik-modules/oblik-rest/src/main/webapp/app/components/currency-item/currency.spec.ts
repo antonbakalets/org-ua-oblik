762a18fe4b361febb2778937edfaf1a10e52bfdc
Anton Bakalets <antonbakalets@yahoo.fr>
2017-04-27 22:44:21 +0300

    Karma unit test configuration.

--- oblik-modules/oblik-rest/src/main/webapp/app/components/currency-item/currency.spec.ts
+++ oblik-modules/oblik-rest/src/main/webapp/app/components/currency-item/currency.spec.ts
@@ -1,8 +1,11 @@
 import CurrencyItemComponent from "./currency";
 
 describe('Currency item', () => {
+
+
+
     it('should be initialized', () => {
-        expect(CurrencyItemComponent).toBeDefined()// The code that tests multiplication goes here
+        expect(new CurrencyItemComponent(null)).toBeDefined()// The code that tests multiplication goes here
     });
     it('should not divide by zero', () => {
         // The code that tests division by zero goes here
---
+++ oblik-modules/oblik-rest/src/main/webapp/karma-system-config.js
@@ -0,0 +1,33 @@
+System.config({
+    baseURL: 'base',
+    transpiler: 'typescript',
+    typescriptOptions: {emitDecoratorMetadata: true},
+    map: {
+        'app' : 'app',
+        'rxjs': 'node_modules/rxjs',
+
+        '@angular/common'                  : 'ng:common/bundles/common.umd.js',
+        '@angular/compiler'                : 'ng:compiler/bundles/compiler.umd.js',
+        '@angular/core'                    : 'ng:core/bundles/core.umd.js',
+        '@angular/forms'                   : 'ng:forms/bundles/forms.umd.js',
+        '@angular/http'                    : 'ng:http/bundles/http.umd.js',
+        '@angular/platform-browser'        : 'ng:platform-browser/bundles/platform-browser.umd.js',
+        '@angular/platform-browser-dynamic': 'ng:platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+        '@angular/router'                  : 'ng:router/bundles/router.umd.js',
+
+        '@angular/common/testing'                  : 'ng:common/bundles/common-testing.umd.js',
+        '@angular/compiler/testing'                : 'ng:compiler/bundles/compiler-testing.umd.js',
+        '@angular/core/testing'                    : 'ng:core/bundles/core-testing.umd.js',
+        '@angular/forms/testing'                   : 'ng:forms/bundles/forms-testing.umd.js',
+        '@angular/http/testing'                    : 'ng:http/bundles/http-testing.umd.js',
+        '@angular/platform-browser/testing'        : 'ng:platform-browser/bundles/platform-browser-testing.umd.js',
+        '@angular/platform-browser-dynamic/testing': 'ng:platform-browser-dynamic/bundles/platform-browser-dynamic-testing.umd.js',
+    },
+    paths: {
+        'ng:': 'node_modules/@angular/'
+    },
+    packages: {
+        'app' : {main: 'main', defaultExtension: 'ts'},
+        'rxjs': {main: 'index'}
+    }
+});
