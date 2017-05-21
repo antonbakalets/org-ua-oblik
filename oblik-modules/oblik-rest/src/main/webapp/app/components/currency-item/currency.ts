885fbb40f79cf00110de5884a49f93c157a972ca
Anton Bakalets <antonbakalets@yahoo.fr>
2017-05-21 23:08:21 +0300

    Reactive currency form validation.

--- oblik-modules/oblik-rest/src/main/webapp/app/components/currency-item/currency.ts
+++ oblik-modules/oblik-rest/src/main/webapp/app/components/currency-item/currency.ts
@@ -4,7 +4,8 @@ import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
     selector: 'oblik-currency-item',
-    templateUrl: 'app/components/currency-item/currency.html'
+    templateUrl: 'app/components/currency-item/currency.html',
+    styleUrls: ['app/components/currency-item/currency.css']
 })
 export default class CurrencyItemComponent {
     @Input() currency: Currency;
@@ -13,12 +14,54 @@ export default class CurrencyItemComponent {
     private currencyModel: FormGroup;
 
     constructor(private currencyService: CurrencyService) {
+    }
+
+    ngOnInit(): void {
         this.currencyModel = new FormGroup({
             'symbol': new FormControl('', [Validators.required, Validators.maxLength(10)]),
             // TODO add symbol already exist validation
             'rate': new FormControl('1', [Validators.required, Validators.pattern("^[0-9]+\.?[0-9]*$")])
         });
+
+        this.currencyModel.valueChanges.subscribe(data => this.onValueChanged(data));
+
+        this.onValueChanged(); // (re)set validation messages now
     }
+
+    onValueChanged(data?: any) {
+        if (!this.currencyModel) {
+            return;
+        }
+        const form = this.currencyModel;
+        for (const field in this.formErrors) {
+            // clear previous error message (if any)
+            this.formErrors[field] = '';
+            const control = form.get(field);
+            if (control && control.dirty && !control.valid) {
+                const messages = this.validationMessages[field];
+                for (const key in control.errors) {
+                    this.formErrors[field] += messages[key] + ' ';
+                }
+            }
+        }
+    }
+
+    formErrors = {
+        'symbol': '',
+        'rate': ''
+    };
+
+    validationMessages = {
+        'symbol': {
+            'required':     'Symbol is required.',
+            'exist':        'Symbol already exist.',
+            'maxlength':    'Currency symbol cannot be more than 10 characters long.'
+        },
+        'rate': {
+            'required':     'Rate is required.',
+            'pattern':      'Decimal only.'
+        }
+    };
 
     public toggleEditing() {
         this.editing = !this.editing;
@@ -31,8 +74,8 @@ export default class CurrencyItemComponent {
     }
 
     public onSubmit() {
-        console.log(this.currencyModel);
-        console.log(this.currency);
+        // console.log(this.currencyModel);
+        // console.log(this.currency);
         if (this.currencyModel.valid) {
             if (this.currency == null) {
                 this.currencyService.insert(
