db165cb848ead01755c565e6feb04216c0643b02
Anton Bakalets <antonbakalets@yahoo.fr>
2017-05-31 23:02:03 +0300

    Simple karma unit test.

--- oblik-modules/oblik-rest/src/main/webapp/app/components/currency-item/currency.spec.ts
+++ oblik-modules/oblik-rest/src/main/webapp/app/components/currency-item/currency.spec.ts
@@ -4,10 +4,11 @@ describe('Currency item', () => {
 
 
 
-    it('should be initialized', () => {
+    /*it('should be initialized', () => {
         expect(new CurrencyItemComponent(null)).toBeDefined()// The code that tests multiplication goes here
-    });
+    });*/
     it('should not divide by zero', () => {
+        expect(1 + 1).toBe(2);
         // The code that tests division by zero goes here
     });
 });
