e41295f3542ac8d4de8815086daccc570f068564
Anton Bakalets <antonbakalets@yahoo.fr>
2017-09-14 09:44:36 +0300

    Enable swagger ui.

--- oblik-modules/oblik-rest/src/main/java/org/ua/oblik/config/ApplicationConfig.java
+++ oblik-modules/oblik-rest/src/main/java/org/ua/oblik/config/ApplicationConfig.java
@@ -1,16 +1,27 @@
 package org.ua.oblik.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "org.ua.oblik.rest.v1")
-public class AppConfig extends WebMvcConfigurerAdapter {
+@ImportResource({
+        "classpath:/org/ua/oblik/context/dao-context.xml",
+        "classpath:/org/ua/oblik/context/jpa-context.xml",
+        "classpath:/org/ua/oblik/context/service-context.xml"
+})
+public class ApplicationConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
@@ -23,6 +34,19 @@ public class AppConfig extends WebMvcConfigurerAdapter {
         registry.addResourceHandler("/node_modules/**").addResourceLocations("node_modules/");
         registry.addResourceHandler("/app/**").addResourceLocations("app/");
         registry.addResourceHandler("/systemjs.config.js").addResourceLocations("systemjs.config.js");
+        registry.addResourceHandler("/swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
 
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
     }
 }
