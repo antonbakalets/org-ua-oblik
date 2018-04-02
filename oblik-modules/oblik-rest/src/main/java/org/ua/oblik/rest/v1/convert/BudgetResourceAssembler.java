0b02331b9342890a259592106f00f4aee5af7046
Anton Bakalets <antonbakalets@yahoo.fr>
2018-04-02 21:37:01 +0300

    Budget resource HATEOAS fix.

--- oblik-modules/oblik-rest/src/main/java/org/ua/oblik/rest/v1/convert/BudgetResourceAssembler.java
+++ oblik-modules/oblik-rest/src/main/java/org/ua/oblik/rest/v1/convert/BudgetResourceAssembler.java
@@ -1,9 +1,5 @@
 package org.ua.oblik.rest.v1.convert;
 
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
-
-import java.util.UUID;
-
 import org.springframework.hateoas.mvc.ResourceAssemblerSupport;
 import org.springframework.stereotype.Component;
 import org.ua.oblik.rest.v1.AccountController;
@@ -12,6 +8,12 @@ import org.ua.oblik.rest.v1.CurrencyController;
 import org.ua.oblik.rest.v1.TransactionController;
 import org.ua.oblik.rest.v1.dto.BudgetResource;
 import org.ua.oblik.service.beans.BudgetVO;
+
+import java.util.UUID;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
 
 @Component
 public class BudgetResourceAssembler extends ResourceAssemblerSupport<BudgetVO, BudgetResource> {
@@ -33,9 +35,9 @@ public class BudgetResourceAssembler extends ResourceAssemblerSupport<BudgetVO, 
         UUID id = entity.getBudgetId();
         BudgetResource resource = createResourceWithId(id, entity);
         resource.add(linkTo(CurrencyController.class, id).withRel("currencies"));
-        resource.add(linkTo(AccountController.class, id).withRel("assets"));
-        resource.add(linkTo(AccountController.class, id).withRel("expenses"));
-        resource.add(linkTo(AccountController.class, id).withRel("incomes"));
+        resource.add(linkTo(methodOn(AccountController.class).getAssets(id)).withRel("assets"));
+        resource.add(linkTo(methodOn(AccountController.class).getExpenses(id)).withRel("expenses"));
+        resource.add(linkTo(methodOn(AccountController.class).getIncomes(id)).withRel("incomes"));
         resource.add(linkTo(TransactionController.class, id).withRel("transactions"));
         return resource;
     }
