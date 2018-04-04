e75a79d362d40a547605b5615ff936f9eae3d57c
Anton Bakalets <antonbakalets@yahoo.fr>
2018-04-04 11:00:29 +0300

    DB migration by flyway.

--- oblik-modules/oblik-dao-hibernate/src/main/resources/db/mysql/10-create-database.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE IF NOT EXISTS oblik_db CHARACTER SET = utf8 COLLATE = utf8_general_ci;
-
-GRANT ALL ON oblik_db.* TO 'oblik_user'@'localhost' IDENTIFIED BY 'oblik_pass';
-
-FLUSH PRIVILEGES;
