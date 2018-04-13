87374adab79b6c4d16cfe358081056df1023b8e9
Anton Bakalets <antonbakalets@yahoo.fr>
2018-04-13 17:57:27 +0300

    Import bootstrap ui.

--- oblik-modules/oblik-frontend/src/app/login/login.component.ts
+++ oblik-modules/oblik-frontend/src/app/login/login.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  targetUrl = '';
+
+  constructor(private router: Router,
+              private route: ActivatedRoute) {
+  }
+
+  ngOnInit() {
+    this.route.queryParams
+      .subscribe(params => this.targetUrl = params['return'] || '/');
+  }
+
+  login() {
+    this.router.navigate([this.targetUrl]);
+  }
 }
